5561100a6cd68a33ccc0857a7ab70ae11ae2279d
Min Htet Myet (Mattral) <88831350+Mattral@users.noreply.github.com>
2023-12-07 23:45:04 +0630

    Update KNNRegressor.py

--- KnearestNeighbour/KNNRegressor.py
+++ KnearestNeighbour/KNNRegressor.py
@@ -16,17 +16,48 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # KNN Regressor
 class KNNRegressor:
     def __init__(self, k=3):
+        """
+        Initialize KNNRegressor.
+
+        Parameters:
+        - k (int): Number of neighbors to consider for prediction.
+        """
         self.k = k
 
     def fit(self, X_train, y_train):
+        """
+        Fit the model with training data.
+
+        Parameters:
+        - X_train (numpy.ndarray): Training feature data.
+        - y_train (numpy.ndarray): Training target data.
+        """
         self.X_train = X_train
         self.y_train = y_train
 
     def predict(self, X_test):
+        """
+        Predict target values for the given test data.
+
+        Parameters:
+        - X_test (numpy.ndarray): Test feature data.
+
+        Returns:
+        - numpy.ndarray: Predicted target values.
+        """
         predictions = [self._predict(x) for x in X_test]
         return np.array(predictions)
 
     def _predict(self, x):
+        """
+        Predict target value for a single data point.
+
+        Parameters:
+        - x (numpy.ndarray): Single data point.
+
+        Returns:
+        - float: Predicted target value.
+        """
         distances = [np.linalg.norm(x - x_train) for x_train in self.X_train]
         k_neighbors_indices = np.argsort(distances)[:self.k]
         k_neighbor_labels = [self.y_train[i] for i in k_neighbors_indices]
