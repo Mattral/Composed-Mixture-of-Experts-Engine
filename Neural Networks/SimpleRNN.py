a243c13c8a24a3726a3722e18a20b0d465d76f4e
Min Htet Myet (Mattral) <88831350+Mattral@users.noreply.github.com>
2023-12-07 19:11:09 +0630

    Update SimpleRNN.py

--- Neural Networks/SimpleRNN.py
+++ Neural Networks/SimpleRNN.py
@@ -1,16 +1,47 @@
+'''
+SimpleRNN: A class representing a simple RNN model with forward pass.
+
+__init__: Initializes the RNN model with weight matrices and biases.
+forward: Performs the forward pass through the RNN and returns the hidden states.
+
+Synthetic Data:
+-Generates synthetic input data and uses the SimpleRNN model to compute hidden states.
+-Prints the hidden states and plots them over timesteps.
+
+Time Series DataSet:
+-Loads Microsoft stock data from a CSV file.
+-Converts the 'Date' column to datetime format.
+-Plots the closing prices of Microsoft stock over time.
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 class SimpleRNN:
     def __init__(self, input_size, hidden_size):
+        """
+        Initialize a simple RNN model.
+
+        Parameters:
+        - input_size (int): The size of the input.
+        - hidden_size (int): The size of the hidden state.
+        """
         self.W_hh = np.eye(hidden_size)
         self.W_xh = np.array([[1]])
         self.b_h = np.zeros((1, hidden_size))
         self.hidden_size = hidden_size
 
     def forward(self, x):
+        """
+        Perform forward pass through the RNN.
+
+        Parameters:
+        - x (numpy array): Input sequence.
+
+        Returns:
+        - list: List of hidden states at each timestep.
+        """
         hidden_states = []
         h_t = np.zeros((1, self.hidden_size))
 
@@ -19,12 +50,10 @@ class SimpleRNN:
             hidden_states.append(h_t.flatten())
 
         return hidden_states
-
-"""
-
------------------ Synthetic Data ------------------------
-
-"""
+    
+#__________________________________________________________________________________
+    
+# Synthetic Data
 # Generate input data
 input_data = np.array([[-1], [0], [1], [2], [3], [4]])
 
@@ -47,13 +76,10 @@ plt.ylim([-1.1, 1.1])  # Adjust the y-axis limits
 plt.legend()
 plt.show()
 
+#_______________________________________________________________________________________
 
 
-"""
------------------- Times Series DataSet -----------------------
-"""
-
-
+# Time Series DataSet
 # Load data from CSV file
 file_path = 'Microsoft_Stock.csv'
 df = pd.read_csv(file_path)
